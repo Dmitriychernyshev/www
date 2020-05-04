a040545b7b7b89987ae0fc34f1669d8a14c4d165
Dmitriychernyshev <64793364+Dmitriychernyshev@users.noreply.github.com>
2020-05-04 18:57:12 +0400

    Add files via upload

---
+++ js.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+      $(".phone").html("+7 (903) 11-11-133");
+      $(".phone").css('font-size', '10px');
+      $(".phone span").css('display', 'none');
+      $(".call").css('border', 'solid 1px pink');
+      $(".call").css('display','inline-block');
+      $(".call span").css('color', 'green');
+    });
